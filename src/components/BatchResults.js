1cbddd4109e868c9815193eb2ebe341e4bd366e8
ryan-rayark <48552331+ryan-rayark@users.noreply.github.com>
2019-12-03 17:29:07 +0530

    batch result update

--- src/components/BatchResults.js
+++ src/components/BatchResults.js
@@ -9,7 +9,7 @@ class BatchResults extends React.Component{
         return(
           <div height = "100%">
               <iframe 
-              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRBE5pQ0ZZ6ZyqjCdel45uJMr7Y5eefcqdosHO8UHaLjRvA3L7EId3cvSYtv5a6M7u0TI6bbP27cfKm/pubhtml?widget=true&amp;headers=false"
+              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRBE5pQ0ZZ6ZyqjCdel45uJMr7Y5eefcqdosHO8UHaLjRvA3L7EId3cvSYtv5a6M7u0TI6bbP27cfKm/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
               width="100%" 
               height="100%"
               height="600px"
